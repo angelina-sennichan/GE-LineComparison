d7d844522e70255ddd6a3e557c9972526c0388d2
angelina <angelinasennichan@gmail.com>
2025-12-10 14:51:17 +0530

    Added: Line Length Calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Line Comparison");
+        System.out.println("Welcome to Line Comparison Computation.");
+        Main m1 = new Main();
+        //constructor - a special method / function used to create object of the class
+        Line l1 = new Line(m1.getPoint(), m1.getPoint());
+        l1.calculateLength();
+    }
+    public Point getPoint(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x co-ordinate: ");
+        double x = sc.nextDouble();
+        System.out.print("Enter y co-ordinate: ");
+        double y = sc.nextDouble();
+        return new Point(x,y);
     }
 }
